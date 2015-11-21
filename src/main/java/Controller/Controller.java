9707081abac1bc9e6f3ce02c20890f8545d7ecc9
Bill Horst <billhorst7@gmail.com>
2015-11-20 21:21:57 -0700

    First commit for Kennel console program.

---
+++ src/main/java/Controller/Controller.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controller;
+
+import com.bill.consoleio.ConsoleIO;
+import java.io.Console;
+
+/**
+ *
+ * @author Bill
+ */
+public class Controller {
+    
+    ConsoleIO io = new ConsoleIO();
+    
+    public void run() {
+        
+        io.printOut("Kennel program start");
+        
+    }
+    
+}
